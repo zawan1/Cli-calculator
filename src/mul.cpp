dc70541ac30722f9eee51da0b0ea4e55025e5f94
Zubair Awan <zawan9624@gmail.com>
2025-10-18 08:48:09 -0700

    multiplication updated

--- src/mul.cpp
+++ src/mul.cpp
@@ -2,5 +2,5 @@
 
 double mul(double a, double b) {
     return a*b;//this will return multiplication value of two numers;
-    /*this is my multiplication module*/
+    /*this is my multiplication module and please check it*/
 }
